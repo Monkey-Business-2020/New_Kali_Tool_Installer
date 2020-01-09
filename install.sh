0d277e94d1bf1eab4456e5075b694e64ca0b6321
Norseman2018 <38373625+Norseman2018@users.noreply.github.com>
2020-01-09 22:03:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ sudo apt install python-pip
 sudo apt install htop
 sudo apt install flameshot	
 cd /opt
-git clone https://github.com/UnaPibaGeek/ctfr.git
-git clone https://github.com/zricethezav/gitleaks.git
-git clone https://github.com/rebootuser/LinEnum.git
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
-git clone https://github.com/dxa4481/truffleHog.git
-git clone https://github.com/trustedsec/unicorn.git
-git clone https://github.com/Veil-Framework/Veil.git
-git clone https://github.com/jordanpotti/AWSBucketDump.git
-git clone https://github.com/FortyNorthSecurity/EyeWitness.git
-git clone https://github.com/aboul3la/Sublist3r.git
-git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/UnaPibaGeek/ctfr.git -y
+git clone https://github.com/zricethezav/gitleaks.git -y
+git clone https://github.com/rebootuser/LinEnum.git -y
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git -y
+git clone https://github.com/dxa4481/truffleHog.git -y
+git clone https://github.com/trustedsec/unicorn.git -y
+git clone https://github.com/Veil-Framework/Veil.git -y
+git clone https://github.com/jordanpotti/AWSBucketDump.git -y
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git -y
+git clone https://github.com/aboul3la/Sublist3r.git -y
+git clone https://github.com/maurosoria/dirsearch.git -y
 bash /opt/EyeWitness/setup/setup.sh
 cd /opt/AWSBucketDump
 pip install -r requirements.txt
